3b73ae441031fcb9f47d177594ced37ca930ba8b
Michael Epelboim <mepelboim@outlook.com>
2023-04-01 18:34:32 +0200

    completed the koa server

--- koa-server/router.js
+++ koa-server/router.js
@@ -2,7 +2,6 @@ let Router = require('koa-better-router');
 let router = Router().loadMethods();
 
 const booksController = require('./controllers/booksController');
-const authorsController = require('./controllers/authorsController');
 
 // Routes:
 // getAllBooks
@@ -24,14 +23,5 @@ router.put('/books/:bookId', booksController.updateOneBook);
 // deleteOneBook
 // /books/:bookId
 router.del('/books/:bookId', booksController.deleteOneBook);
-
-// getBooksByAuthor
-// books/?author=:authorName
-// router.get('/books/', );
-
-// router.get('/', (ctx, next) => {
-//   ctx.body = `Hello world! Prefix: ${ctx.route.prefix}`;
-//   return next();
-// });
 
 module.exports = router;
